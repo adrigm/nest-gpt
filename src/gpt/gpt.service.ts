bd98196e1b7d4884d3936d2142fa67adeee8801d
adrigm <adrigm.admin@gmail.com>
2024-01-16 23:24:37 +0100

    added ortography case

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { orthograpyUseCase } from './use-cases';
 import { OrthograpyDto } from './dtos';
+import OpenAI from 'openai';
 
 @Injectable()
 export class GptService {
+  private openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
   async orthograpyCheck(orthograpyDto: OrthograpyDto) {
-    return await orthograpyUseCase({ promt: orthograpyDto.promt });
+    return await orthograpyUseCase(this.openai, { promt: orthograpyDto.promt });
   }
 }
--- src/gpt/use-cases/orthograpy.use-case.ts
+++ src/gpt/use-cases/orthograpy.use-case.ts
@@ -1,11 +1,46 @@
+import OpenAI from 'openai';
+
 interface Options {
   promt: string;
 }
 
-export const orthograpyUseCase = async (options: Options) => {
+export const orthograpyUseCase = async (openai: OpenAI, options: Options) => {
   const { promt } = options;
 
-  return {
-    promt: promt,
-  };
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content: `
+        Se te darán textos con errores ortográficos y gramaticales y deberás corregirlos. Debes responder al usuario en formato JSON. Debes corregirlos y retornar las correcciones. también debes dar un porcentaje de aciertos basandote en el numero de errores y la longitud del texto.
+
+        Si no hay errores debes retornar un mensaje de felicitaciones y un porcentaje de aciertos de 100%.
+
+        Ejemplo de salida:
+        {
+            userScore: number,
+            errors: string[], // ['error -> solución', 'error -> solución']
+            message: string, // Usa emojis y texto para felicitar al usuario
+        }
+
+        `,
+      },
+      {
+        role: 'user',
+        content: promt,
+      },
+    ],
+    model: 'gpt-4',
+    temperature: 0.3,
+    max_tokens: 150,
+    // response_format: {
+    //   type: 'json_object',
+    // },
+  });
+
+  console.log(completion);
+
+  const jsonResponse = JSON.parse(completion.choices[0].message.content);
+
+  return jsonResponse;
 };
